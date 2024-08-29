463fa1277d21d7c40afe97ac5b648273310b6bee
Maratib Ali Khan <maratib@gmail.com>
2024-08-29 21:51:20 +0500

    App.tsx updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import Hello from "./components/Hello";
 
 function App() {
   return (
-    <>
+    <div>
       <h1 className="text-red-500">Welcome to React With Tailwind</h1>
       <Hello />
-    </>
+    </div>
   );
 }
 
